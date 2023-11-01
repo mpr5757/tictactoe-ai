faf5a3509ecd51633f06f37190e20452c0021171
Rafshan Rehan <mpr5757@psu.edu>
2023-11-01 00:08:34 -0400

    updated engine.py to work and added comments, but victory message is unchanged

--- tictactoe/engine.py
+++ tictactoe/engine.py
@@ -20,40 +20,43 @@ class Engine:
         if board.is_gameover() or depth >= self.level:
             return self.evaluate_board(board, depth), None
 
+    #Originally, the AI would want to maximize itself and minimize the player 
+    #Now, we want it so that the AI tries ti minimize itself and maximize the player, so we reverse the following if/else statement 
+    #i.e. we change the mins to max and we change the max to mins 
         if ai_turn:
-            max_eval = float('-inf')
+            min_eval = float('inf')
             best_move = None
             for move in available_moves:
                 board.push(move, self.ai)
                 eval_ = self.minimax(board, False, depth + 1, alpha, beta)[0]
                 board.undo(move)
-                max_eval = max(max_eval, eval_)
-                if max_eval == eval_:
+                min_eval = min(min_eval, eval_)
+                if min_eval == eval_:
                     best_move = move
-                alpha = max(alpha, max_eval)
+                alpha = min(alpha, min_eval)
                 if alpha > beta:
-                    return max_eval, best_move
-            return max_eval, best_move
+                    return min_eval, best_move
+            return min_eval, best_move
         else:
-            min_eval = float('inf')
+            max_eval = float('-inf')
             best_move = None
             for move in available_moves:
                 board.push(move, self.foe)
                 eval_ = self.minimax(board, True, depth + 1, alpha, beta)[0]
                 board.undo(move)
-                min_eval = min(min_eval, eval_)
-                if min_eval == eval_:
+                max_eval = max(max_eval, eval_)
+                if max_eval == eval_:
                     best_move = move
-                beta = min(min_eval, beta)
+                beta = max(max_eval, beta)
                 if beta < alpha:
-                    return min_eval, best_move
-            return min_eval, best_move
+                    return max_eval, best_move
+            return max_eval, best_move
 
     def evaluate_board(self, board: Board, depth: int) -> Score:
         if board.winner() == self.ai:
-            return board.size**2 - depth
+            return depth + board.size**2 
         elif board.winner() == self.foe:
-            return -1 * board.size**2 - depth
+            return depth - board.size**2 
         return 0
 
     def evaluate_best_move(self, board: Board) -> Square:
